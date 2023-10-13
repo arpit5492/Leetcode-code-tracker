09c9392f59bb4aeed3175e60ea85f9d848d42549
Arpit Mallick <arpit@Arpits-Air.lan>
2023-10-13 12:28:07 -0700

    Matrix Diagonal Sum

--- SumDiag.py
+++ SumDiag.py
@@ -1,11 +1,25 @@
 def SumDiag(matrix):
-    add = 0
+    t_sum = 0
+    # for i in range(len(matrix)):
+    #     # for j in range(len(matrix[0])):
+    #     #     if i == j:
+    #     #         add+=matrix[i][j]
+    #     add+=matrix[i][i]
+    # return add
     for i in range(len(matrix)):
-        # for j in range(len(matrix[0])):
-        #     if i == j:
-        #         add+=matrix[i][j]
-        add+=matrix[i][i]
-    return add
+            # for adding primary diagonal elements
+        t_sum += matrix[i][i]
+            # for adding secondary diagonal elements
+        t_sum += matrix[len(matrix)-1-i][i]
+
+    if len(matrix) % 2 != 0: t_sum -= matrix[len(matrix)//2][len(matrix)//2]
+    return t_sum
+
+# print(SumDiag([[1,2,3,4,5],
+#                [4,5,6,7,8],
+#                [7,8,9,10,11,12],
+#                [1,2,3,4,5],
+#                [1,2,3,4,5]]))
 
 print(SumDiag([[1,2,3],
                [4,5,6],
