96d4e37d9ad6589fa8c3914c673ccf5b907b79f6
Arpit Mallick <arpit@Arpits-Air.lan>
2023-10-24 20:47:17 -0700

    Remove duplicates from sorted array

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -1,9 +1,20 @@
 def RemoveDup(arr):
-    new_list = []
-    FoundItems = set()
-    for item in arr:
-        if item not in FoundItems:
-            new_list.append(item)
-            FoundItems.add(item)
-    return (new_list)
-print(RemoveDup([0,0,1,1,1,2,2,3,3,4]))
+    # start pointer
+    i = 0
+    # end pointer
+    j = i+1
+    while(i<=j and j<len(arr)):        # 0 1 2 3 4 5 6 7 8 9
+        if(arr[i] == arr[j]):          # 0 1 2 3 4
+                                       #         i j
+            j+=1
+        else:
+            arr[i+1]=arr[j]
+            i+=1
+    return i+1
+        
+# print(RemoveDup([0,0,1,1,1,2,2,3,3,4]))
+print(RemoveDup([1,2,3]))
+
+# 0 1 2 -> index
+# 1 2 3 -> array
+#     i j
